ff84addcbf6dff09842993c8fdc3882e21c54880
Karthikeyan <xmenv22222@gmail.com>
2020-10-12 19:24:53 +0530

    Updates

---
+++ precache-manifest.ee7b07c9f69b10f0296caf5a217d9a88.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eae0994d63827c9a6be8d86f6175779b",
+    "url": "/Splash/index.html"
+  },
+  {
+    "revision": "cc08d431b5de7af4b098",
+    "url": "/Splash/static/css/2.2ce93ba3.chunk.css"
+  },
+  {
+    "revision": "262ff150874f18932091",
+    "url": "/Splash/static/css/main.f16f9333.chunk.css"
+  },
+  {
+    "revision": "cc08d431b5de7af4b098",
+    "url": "/Splash/static/js/2.82c46b9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Splash/static/js/2.82c46b9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "262ff150874f18932091",
+    "url": "/Splash/static/js/main.8756a4d2.chunk.js"
+  },
+  {
+    "revision": "30686d10f5373123296f",
+    "url": "/Splash/static/js/runtime-main.f97666dc.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/Splash/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
--- src/emoji_picker.jsx
+++
@@ -1,2 +0,0 @@
-import React,{useState} from "react";
-import Picker from "emoji-picker-react";
